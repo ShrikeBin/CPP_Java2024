2ac8c4e1d0dc61c460d3722d9a094cfba40fc111
ShrikeBin <grahamka.ha@gmail.com>
2024-04-01 10:46:49 +0200

    one mistake in Rectangle.java?

--- list3/java1/Rectangle.java
+++ list3/java1/Rectangle.java
@@ -7,29 +7,75 @@ public class Rectangle extends Quadrilateral
             throw new IllegalArgumentException("It's too many sides for a Rectangle: " + input_sides.length);
         }
 
+        if (input_sides.length < 4)
+        {
+            throw new IllegalArgumentException("It's too few sides for a Rectangle: " + input_sides.length);
+        }
+
         for (int i = 0; i<4; i++)
         {
             if (input_sides[i] < 0)
             {
                 throw new IllegalArgumentException("Sides cannot be less than 0, provided: "+ input_sides[i]);
             }
+        }
 
-            if (i<3 && !(input_sides[i]==input_sides[i+1]))
+        //potworek do sprawdzania czy to prostokąt :D
+
+        double checker = input_sides[0];
+        double checker_alt = 0;
+
+        for(int i = 0; i < 4; i++)
+        {
+            if(!(input_sides[0]==input_sides[i]))
             {
-                throw new IllegalArgumentException("This is not a Rectangle");
+                checker_alt = input_sides[i];
+                break;
             }
-            
-            this.sides[i] = input_sides[i];
+        }
+
+        int counter = 0;
+        int counter_alt = 0;
+
+        for(double index : input_sides)
+        {
+            if (index == checker)
+            {
+                counter ++;
+            }
+
+            if (index == checker_alt)
+            {
+                counter_alt ++;
+            }
+        }
+
+        if(!(counter_alt==counter))
+        {
+            throw new IllegalArgumentException("This is not a Rectangle, sides have different sizes: " + input_sides[0] + ":" + input_sides[1] + ":" + input_sides[2] + ":" + input_sides[3]);
+        }
+        else
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                sides[i] = input_sides[i];
+            }
         }
     }
 
     public double field()
     {
-        return sides[0] * sides[0];
+        for(int i = 0; i < 4; i++)
+        {
+            if(!(sides[0]==sides[i]))
+            {
+                return sides[0]*sides[i];
+            }
+        }
     }
 
     public double lenght()
     {
-        return sides[0] * 4;
+        return sides[0] + sides[1] + sides[2] + sides[3];
     }
 }
--- list3/java1/Rhombus.java
+++ list3/java1/Rhombus.java
@@ -1,4 +1,56 @@
-public class Rhombus 
+import java.lang.Math;
+
+public class Rhombus extends Quadrilateral
 {
-    
+    public Rhombus(double[] input_sides, double angle) throws IllegalArgumentException
+    {
+        if (input_sides.length > 4)
+        {
+            throw new IllegalArgumentException("It's too many sides for a Rhombus: " + input_sides.length);
+        }
+
+        if (input_sides.length < 4)
+        {
+            throw new IllegalArgumentException("It's too few sides for a Rhombus: " + input_sides.length);
+        }
+
+        if (angle <= 0)
+        {
+            throw new IllegalArgumentException("Angle has to be greater than 0, provided: "+ angle);
+        }
+
+        for (int i = 0; i<4; i++)
+        {
+            if (input_sides[i] < 0)
+            {
+                throw new IllegalArgumentException("Sides cannot be less than 0, provided: "+ input_sides[i]);
+            }
+
+            if (i<3 && !(input_sides[i]==input_sides[i+1]))
+            {
+                throw new IllegalArgumentException("This is not a Rhombus, sides have different sizes: " + input_sides[0] + ":" + input_sides[1] + ":" + input_sides[2] + ":" + input_sides[3]);
+            }
+            
+            this.sides[i] = input_sides[i];
+        }
+
+        if (angle > 90)
+        {
+            this.angle = 180 - angle;
+        }
+        else
+        {
+            this.angle = angle;
+        }
+    }
+
+    public double field()
+    {
+        return (Math.sin(Math.toRadians(this.angle))) * sides[0] * sides[0];
+    }
+
+    public double lenght()
+    {
+        return sides[0] * 4;
+    }
 }
