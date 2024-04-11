1b0f524d5eb1055ea344194b7e65406502b6a36f
ShrikeBin <grahamka.ha@gmail.com>
2024-04-11 15:40:09 +0200

    quick fixes

--- list3/java/task1/Figure.java
+++ list3/java/task1/Figure.java
@@ -1,7 +1,4 @@
 public abstract class Figure implements Methods
 {
-    protected Figure() throws InstantiationError
-    {
-        //throw new InstantiationError("This is an abstract class you moron"); // może zostaw puste...
-    }
+
 }
