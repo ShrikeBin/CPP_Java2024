eb0076c72d5bb3a8f4dcca74dfb087f068ccf325
ShrikeBin <grahamka.ha@gmail.com>
2024-06-11 19:14:11 +0200

    final fixes hopefully

--- list6/Cell.java
+++ list6/Cell.java
@@ -162,27 +162,27 @@ public class Cell implements Runnable
                                 { 
                                     for(Cell neighbor  : neighbors)
                                     {
-                                        synchronized(neighbor) // to ensure synchronization
+                                        if(neighbor.isActive()) // to ensure synchronization
                                         {
                                             avgRed[0] += neighbor.getColor().getRed();
                                             avgGreen[0] += neighbor.getColor().getGreen();
                                             avgBlue[0] += neighbor.getColor().getBlue();
                                             ++count[0];
                                         }
+
+                                        if(count[0] != 0)
+                                        {
+                                            changeColor(new Color(avgRed[0] / count[0], avgGreen[0] / count[0], avgBlue[0] / count[0], 1.0));
+                                        }
+                                        else
+                                        {
+                                            changeColor(random.nextColor());
+                                        }
                                     }
                                 }
                             }
                         }
                     }  
-
-                    if(count[0] != 0)
-                    {
-                        changeColor(new Color(avgRed[0] / count[0], avgGreen[0] / count[0], avgBlue[0] / count[0], 1.0));
-                    }
-                    else
-                    {
-                        changeColor(random.nextColor());
-                    }
                 }
                 //MyLogger.logger.log(Level.FINE, "End: " + thread.threadId());
                 System.out.println("End: " + thread.threadId());
